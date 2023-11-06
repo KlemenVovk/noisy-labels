60369dd4d66d749b8ff91bc4dd2b1056c0294a63
Ziga R <ziga.mlearning@gmail.com>
2023-11-06 18:20:25 +0100

    added missing typehints

--- src/data/noisylabels.py
+++ src/data/noisylabels.py
@@ -23,11 +23,11 @@ class NoisyLabels:
     }
 
     @classmethod
-    def get_filename(cls, dataset: Literal["cifar10", "cifar100"]):
+    def get_filename(cls, dataset: Literal["cifar10", "cifar100"]) -> str:
         return f"{cls._name_map[dataset]}_human.pt"
 
     @classmethod
-    def get_url(cls, dataset: Literal["cifar10", "cifar100"]):
+    def get_url(cls, dataset: Literal["cifar10", "cifar100"]) -> str:
         return f"https://github.com/UCSC-REAL/cifar-10-100n/raw/main/data/{cls.get_filename(dataset)}"
 
     def __init__(self, dataset: Literal["cifar10", "cifar100"], dir: str, download: bool) -> None:
@@ -77,7 +77,7 @@ class NoisyLabelsCIFAR10Dataset(CIFAR10):
             cifar_dir: str,
             download: bool,
             *args, **kwargs,
-            ):
+            ) -> None:
         super().__init__(cifar_dir, *args, download=download, train=True, **kwargs)
         noise_dict = NoisyLabels("cifar10", noise_dir, download).load()
         self.noisy_targets = noise_dict[noise_type]
@@ -101,7 +101,7 @@ class NoisyLabelsCIFAR100Dataset(CIFAR100):
             cifar_dir: str,
             download: bool,
             *args, **kwargs,
-            ):
+            ) -> None:
         super().__init__(cifar_dir, *args, download=download, train=True, **kwargs)
         noise_dict = NoisyLabels("cifar100", noise_dir, download).load()
         self.noisy_targets = noise_dict[noise_type]
@@ -114,11 +114,11 @@ class NoisyLabelsCIFAR100Dataset(CIFAR100):
 class NoisylabelsDataModule(L.LightningDataModule):
 
     @property
-    def num_classes(self):
+    def num_classes(self) -> int:
         return 10 if self.dataset_name == "cifar10" else 100
     
     @property
-    def num_training_samples(self):
+    def num_training_samples(self) -> int:
         return 50000
     
     def __init__(
@@ -130,7 +130,7 @@ class NoisylabelsDataModule(L.LightningDataModule):
             noisylabels_dir="../data/noisylabels",
             batch_size=64,
             num_workers=4,
-            ):
+            ) -> None:
         super().__init__()
         self.train_transform = train_transform
         self.test_transform = test_transform
@@ -143,7 +143,7 @@ class NoisylabelsDataModule(L.LightningDataModule):
         self.batch_size = batch_size
         self.num_workers = num_workers
 
-    def prepare_data(self):
+    def prepare_data(self) -> None:
         # Download CIFAR and noisylabels data if needed
         NoisyLabels(self.dataset_name, self.noisylabels_dir, True)
         if self.dataset_name == "cifar10":
@@ -152,7 +152,7 @@ class NoisylabelsDataModule(L.LightningDataModule):
             CIFAR100(root=self.cifar_dir, train=False, download=True)
 
     
-    def setup(self, stage=None):
+    def setup(self, stage=None) -> None:
         if stage == 'fit' or stage is None:
             if self.dataset_name == "cifar10":
                 self.train_dataset = NoisyLabelsCIFAR10Dataset(self.noise_type, self.noisylabels_dir, self.cifar_dir, False, transform=self.test_transform)
@@ -161,11 +161,11 @@ class NoisylabelsDataModule(L.LightningDataModule):
                 self.train_dataset = NoisyLabelsCIFAR100Dataset(self.noise_type, self.noisylabels_dir, self.cifar_dir, False, transform=self.test_transform)
                 self.test_dataset = CIFAR100(self.cifar_dir, train=False, transform=self.test_transform)
 
-    def train_dataloader(self):
+    def train_dataloader(self) -> DataLoader:
         return DataLoader(self.train_dataset, batch_size=self.batch_size, num_workers=self.num_workers)
 
-    def val_dataloader(self):
+    def val_dataloader(self) -> DataLoader:
         return DataLoader(self.test_dataset, batch_size=self.batch_size, num_workers=self.num_workers)
 
-    def test_dataloader(self):
+    def test_dataloader(self) -> DataLoader:
         return DataLoader(self.test_dataset, batch_size=self.batch_size, num_workers=self.num_workers)
