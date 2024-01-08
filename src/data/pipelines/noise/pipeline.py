5c1e5d74e9f6df0840a340963e22dd7a36fe55b0
Ziga R <ziga.mlearning@gmail.com>
2024-01-08 13:47:23 +0100

    added option to keep original label

--- src/data/pipelines/noise/pipeline.py
+++ src/data/pipelines/noise/pipeline.py
@@ -3,13 +3,21 @@ from ..base import AugmentationPipeline
 
 
 class NoisePipeline(AugmentationPipeline):
+    """Augments the dataset by transforming labels with Noise.
 
-    def __init__(self, noise: Noise) -> None:
+    Args:
+        noise (Noise): Noise to transform the labels with.
+        keep_original (bool): Whether to return original label as well or not.
+    """
+
+    def __init__(self, noise: Noise, keep_original: bool = False) -> None:
         super().__init__()
         self.noise = noise
+        self.keep_original = keep_original
 
     def transform(self, dataset_cls):
         noise_ = self.noise
+        keep_ = self.keep_original
 
         class NoisyDataset(dataset_cls):
 
@@ -20,6 +28,8 @@ class NoisePipeline(AugmentationPipeline):
             def __getitem__(self, index):
                 # add noise to targets
                 (x, y, *other) = super().__getitem__(index)
+                if keep_:
+                    return x, self.noise(x, y, index), y, *other    
                 return x, self.noise(x, y, index), *other
 
         return NoisyDataset
