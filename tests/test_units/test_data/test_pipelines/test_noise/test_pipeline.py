7cd44c5c371c99fc3b98187739647287ca439821
Ziga R <ziga.mlearning@gmail.com>
2024-01-09 15:51:31 +0100

    added unit tests for pipelines

---
+++ tests/test_units/test_data/test_pipelines/test_noise/test_pipeline.py
@@ -0,0 +1,51 @@
+import torch
+from PIL import Image
+
+from src.data.pipelines.noise.pipeline import AddNoise
+from src.data.pipelines.noise.noises import InstanceNoise
+
+from ..common import CIFAR10, CIFAR10WithExtras, args
+
+
+def test_simple():
+    pipe = AddNoise(InstanceNoise(torch.tensor([1, 2, 3])))
+    dataset_cls = pipe(CIFAR10)
+    dataset = dataset_cls(**args)
+    sample = dataset[0]
+
+    # check types
+    assert isinstance(sample[0], Image.Image)
+    assert isinstance(sample[1], int)
+
+    # check noise
+    for i in range(3):
+        assert dataset[i][1] == i+1
+
+def test_extras():
+    pipe = AddNoise(InstanceNoise(torch.tensor([1, 2, 3])))
+    dataset_cls = pipe(CIFAR10WithExtras)
+    dataset = dataset_cls(**args)
+    sample = dataset[0]
+
+    # check types
+    assert isinstance(sample[0], Image.Image)
+    assert isinstance(sample[1], int)
+
+    # check extras
+    assert sample[2] == "something"
+    assert sample[3] == "something_else"
+
+    # check noise
+    for i in range(3):
+        assert dataset[i][1] == i+1
+
+def test_persistance():
+    pipe = AddNoise(InstanceNoise(torch.tensor([1, 2, 3])))
+    dataset_cls1 = pipe(CIFAR10)
+    dataset_cls2 = pipe(CIFAR10)
+
+    dataset1 = dataset_cls1(**args)
+    dataset2 = dataset_cls2(**args)
+    
+    for i in range(3):
+        assert dataset1[i][1] == dataset2[i][1] == i+1
