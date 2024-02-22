be2c2f700c13e78b99e63fe97857a552fbc96617
ValterH <valter.hudovernik@gmail.com>
2024-02-22 14:02:36 +0100

    reproduce ELR

---
+++ src/configs/methods/ELR/elr_cifar10_noise.py
@@ -0,0 +1,29 @@
+from pytorch_lightning.loggers import CSVLogger
+
+from data.pipelines.base import Compose
+from data.pipelines.index import AddIndex
+from data.pipelines.noise.pipeline import AddNoise
+from data.pipelines.noise.noises import BiasedSymmetricNoise
+
+from configs.data.cifar10 import cifar10_base_config
+from configs.methods.ELR.elr_cifar10_clean import elr_cifar10_clean
+
+class cifar10_index_noise_config(cifar10_base_config):
+    dataset_train_augmentation = Compose([
+        AddNoise(noise=BiasedSymmetricNoise(num_classes=10, num_samples=50000, noise_rate=0.8)), 
+        AddIndex()])
+
+class elr_cifar10_noise(elr_cifar10_clean):
+
+    data_config = cifar10_index_noise_config
+
+    learning_strategy_args = dict(beta = 0.7, lmbd=3)  # β ∈ {0.5, 0.7, 0.9, 0.99}, λ ∈ {1, 3, 5, 7, 10}
+
+    trainer_args = dict(
+        max_epochs=150,
+        deterministic=True,
+        # TODO: move to logs when we are not running from src/
+        logger=CSVLogger("../logs", name="elr_symmetric")
+    )
+
+    seed = 1337
---
+++ src/methods/learning_strategies/ELR/utils.py
@@ -0,0 +1,159 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class elr_loss(nn.Module):
+    def __init__(self, num_examp, num_classes=10, lmbd = 3, beta=0.7):
+        """Early Learning Regularization.
+        Parameters
+        * `num_examp` Total number of training examples.
+        * `num_classes` Number of classes in the classification problem.
+        * `lambda` Regularization strength; must be a positive float, controling the strength of the ELR.
+        * `beta` Temporal ensembling momentum for target estimation.
+        """
+        super().__init__()
+        self.num_classes = num_classes
+        self.USE_CUDA = torch.cuda.is_available()
+        self.target = torch.zeros(num_examp, self.num_classes).cuda() if self.USE_CUDA else torch.zeros(num_examp, self.num_classes)
+        self.beta = beta
+        self.lmbd = lmbd
+        
+
+    def forward(self, index, output, label):
+        r"""Early Learning Regularization.
+         Args
+         * `index` Training sample index, due to training set shuffling, index is used to track training examples in different iterations.
+         * `output` Model's logits, same as PyTorch provided loss functions.
+         * `label` Labels, same as PyTorch provided loss functions.
+         """
+
+        y_pred = F.softmax(output,dim=1)
+        y_pred = torch.clamp(y_pred, 1e-4, 1.0-1e-4)
+        y_pred_ = y_pred.data.detach()
+        self.target[index] = self.beta * self.target[index] + (1-self.beta) * ((y_pred_)/(y_pred_).sum(dim=1,keepdim=True))
+        ce_loss = F.cross_entropy(output, label)
+        elr_reg = ((1-(self.target[index] * y_pred).sum(dim=1)).log()).mean()
+        final_loss = ce_loss +  self.lmbd * elr_reg
+        return  final_loss
+    
+
+def sigmoid_rampup(current, rampup_length):
+    """Exponential rampup from  2"""
+    if rampup_length == 0:
+        return 1.0
+    else:
+        current = torch.clip(torch.Tensor([current]), min=0.0, max=rampup_length)
+        phase = 1.0 - current / rampup_length
+        return float(torch.exp(-5.0 * phase * phase))
+    
+
+# Authors ResNet34
+
+def conv3x3(in_planes, out_planes, stride=1):
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
+    
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion*planes)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, self.expansion*planes, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(self.expansion*planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion*planes)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = F.relu(self.bn2(self.conv2(out)))
+        out = self.bn3(self.conv3(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(ResNet, self).__init__()
+        self.in_planes = 64
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.linear = nn.Linear(512*block.expansion, num_classes)
+
+
+        self.gradients = None
+
+
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def activations_hook(self, grad):
+        self.gradients = grad
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 4)
+        y = out.view(out.size(0), -1)
+        out = self.linear(y)
+        if out.requires_grad:
+            out.register_hook(self.activations_hook)
+        return out
+
+    def get_activations_gradient(self):
+        return self.gradients
+
+
+def ResNet34():
+    return ResNet(BasicBlock, [3,4,6,3])
+
+
+def resnet34(num_classes=10):
+    return ResNet(BasicBlock, [3,4,6,3], num_classes=num_classes)
