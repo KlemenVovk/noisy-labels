b408070b2f375dd32de65821d69b567dd6caf828
ValterH <valter.hudovernik@gmail.com>
2024-07-09 10:49:11 +0200

    update CAL config warmup as described in the paper

--- src/reproducibility/noisy_labels/configs/paper_configs/CAL.py
+++ src/reproducibility/noisy_labels/configs/paper_configs/CAL.py
@@ -12,19 +12,19 @@ class CAL_config(BasePaperMethod):
     learning_strategy_cls = CAL
     learning_strategy_args = dict(
         alpha = 0.0, 
-        warmup_epochs = 65,
+        warmup_epochs = 100,
         alpha_scheduler_cls = SegAlpha,
         alpha_scheduler_args = dict(
             alpha_list = [0.0, 1.0, 1.0],
             milestones = [10, 40, 80],
         ),
         alpha_scheduler_args_warmup = dict(
-            alpha_list = [0.0, 2.0],
-            milestones = [10, 40],
+            alpha_list = [0.0, 2.0, 2.0],
+            milestones = [10, 40, 80],
         ),
     )
 
     trainer_args = {
         **BasePaperMethod.trainer_args,
-        "max_epochs": 165,
+        "max_epochs": 200,
     }
