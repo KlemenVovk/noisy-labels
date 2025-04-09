eaf45bc4723959925bb7ae15ab8957b85407009e
ValterH <valter.hudovernik@gmail.com>
2025-04-09 12:14:41 +0200

    update memorization experiment

--- src/reproducibility/memorization_effects/memo.py
+++ src/reproducibility/memorization_effects/memo.py
@@ -1,3 +1,4 @@
+import os
 import argparse
 
 import torch
@@ -6,10 +7,10 @@ from torch.nn import functional as F
 from torchvision.datasets import CIFAR10
 from torchvision import transforms
 from tqdm import tqdm
+import lightning as L
 
-from utils import ResNet34 as resnet34
+from noisypy.methods.classifiers.resnet import ResNet34 as resnet34
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
 
 argparser = argparse.ArgumentParser()
 argparser.add_argument(
@@ -18,16 +19,35 @@ argparser.add_argument(
     help="noise label type",
     choices=["aggre_label", "random_label1", "worse_label"],
 )
+argparser.add_argument(
+    "--scheduler", default="exponential", choices=["exponential", "step"]
+)
+argparser.add_argument("--weight-decay", type=float, default=1e-4, help="weight decay")
 argparser.add_argument("--seed", type=int, default=1, help="random seed")
 argparser.add_argument("--device", type=int, default=0, help="device")
+argparser.add_argument(
+    "--initial-lr", type=float, default=1, help="initial learning rate"
+)
 
 args = argparser.parse_args()
 seed = args.seed
 noise_label = args.noise_label
+
+L.seed_everything(args.seed)
+
+save_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), args.scheduler)
+if args.weight_decay != 1e-4:
+    save_dir += f"_wd_{args.weight_decay}"
+if args.initial_lr != 1:
+    save_dir += f"_lr_{args.initial_lr}"
+os.makedirs(save_dir, exist_ok=True)
+
 device = f"cuda:{args.device}" if torch.cuda.is_available() else "cpu"
 
 noisy_path = "../../../data/noisylabels/CIFAR-10_human.pt"
-noisy_labels = {k: torch.tensor(v) for k, v in torch.load(noisy_path).items()}
+noisy_labels = {
+    k: torch.tensor(v) for k, v in torch.load(noisy_path, weights_only=False).items()
+}
 
 
 cifar10_train_transform = transforms.Compose(
@@ -43,7 +63,6 @@ clean_dataset = CIFAR10(
     "../../../data/cifar", train=True, transform=cifar10_train_transform
 )
 clean_labels = torch.tensor(clean_dataset.targets)
-clean_labels
 
 
 def generate_synthetic_labels(clean_labels, noisy_labels, seed=1337):
@@ -62,6 +81,46 @@ def generate_synthetic_labels(clean_labels, noisy_labels, seed=1337):
     return synthetic_labels
 
 
+def generate_pairflip_labels(clean_labels, noisy_labels, seed=1337):
+    noise_rate = (clean_labels != human_labels).float().mean()
+    # generate transition matrix T
+    num_classes = 10
+    T = torch.zeros(num_classes, num_classes)
+    T[torch.eye(num_classes, dtype=torch.bool)] = 1 - noise_rate
+    for i in range(num_classes):
+        clean_mask = clean_labels == i
+        # Estimate the original transition vector
+        t = torch.zeros(num_classes)
+        for j in range(num_classes):
+            if i == j:
+                continue
+            p = (noisy_labels[clean_mask] == j).float().mean()
+            t[j] = p
+        # Select the most likely transition class
+        j = t.argmax()
+        T[i, j] = noise_rate
+
+    assert torch.isclose(T.sum(axis=1), torch.ones(num_classes)).all()
+    # sample the transition matrix
+    gen = torch.Generator("cpu").manual_seed(seed)
+    synthetic_labels = torch.multinomial(T[clean_labels], 1, generator=gen).squeeze()
+    return synthetic_labels
+
+
+def generate_symmetric_labels(clean_labels, noisy_labels, seed=1337):
+    noise_rate = (clean_labels != noisy_labels).float().mean()
+    # generate transition matrix T
+    num_classes = 10
+    T = torch.ones(num_classes, num_classes) * (noise_rate / (num_classes - 1))
+    T[torch.eye(num_classes, dtype=torch.bool)] = 1 - noise_rate
+
+    assert torch.isclose(T.sum(axis=1), torch.ones(num_classes)).all()
+    # sample the transition matrix
+    gen = torch.Generator("cpu").manual_seed(seed)
+    synthetic_labels = torch.multinomial(T[clean_labels], 1, generator=gen).squeeze()
+    return synthetic_labels
+
+
 def train_and_record_memorization(clean_labels, noisy_labels, num_epochs):
     dataset = clean_dataset
     dataset.targets = noisy_labels
@@ -69,16 +128,23 @@ def train_and_record_memorization(clean_labels, noisy_labels, num_epochs):
 
     model = resnet34(num_classes=10).to(device)
     criterion = nn.CrossEntropyLoss()
-    # use the same optimizer as in the CE config
-    optimizer = torch.optim.SGD(model.parameters(), lr=1, weight_decay=1e-4)
-    # Xiu et al. 2021 use epoch//100 and train for 300 epochs
-    # We need a smooth schedule and train for 150 epochs hence:
-    scheduler = torch.optim.lr_scheduler.LambdaLR(
-        optimizer, lr_lambda=lambda epoch: 0.1 ** (epoch / 50)
+    optimizer = torch.optim.SGD(
+        model.parameters(), lr=args.initial_lr, weight_decay=args.weight_decay
     )
+    if args.scheduler == "exponential":
+        # Xiu et al. 2021 use epoch//100 and train for 300 epochs
+        # We need a smooth schedule and train for 150 epochs hence:
+        scheduler = torch.optim.lr_scheduler.LambdaLR(
+            optimizer, lr_lambda=lambda epoch: 0.1 ** (epoch / 50)
+        )
+    elif args.scheduler == "step":
+        # Use the original step scheduler
+        scheduler = torch.optim.lr_scheduler.MultiStepLR(
+            optimizer, milestones=[50], gamma=0.1
+        )
 
     loader = torch.utils.data.DataLoader(
-        clean_dataset, batch_size=128, shuffle=False, num_workers=8, pin_memory=True
+        dataset, batch_size=128, shuffle=False, num_workers=8, pin_memory=True
     )
 
     clean_memos = []
@@ -95,15 +161,20 @@ def train_and_record_memorization(clean_labels, noisy_labels, num_epochs):
             optimizer.step()
 
             probs.append(F.softmax(y_pred, -1))
-            pbar.set_postfix({"loss": f"{loss.item():.2e}"})
+            pbar.set_postfix(
+                {
+                    "loss": f"{loss.item():.2e}",
+                    "lr": f"{scheduler.get_last_lr()[0]:.2e}",
+                }
+            )
 
         probs = torch.cat(probs)
-        clean_idxs = clean_labels == noisy_labels
+        clean_mask = clean_labels == noisy_labels
         clean_frac = (
-            (probs[clean_idxs].max(axis=-1)[0] > confidence_threshold).float().mean()
+            (probs[clean_mask].max(axis=-1)[0] > confidence_threshold).float().mean()
         )
         wrong_frac = (
-            (probs[~clean_idxs].max(axis=-1)[0] > confidence_threshold).float().mean()
+            (probs[~clean_mask].max(axis=-1)[0] > confidence_threshold).float().mean()
         )
         clean_memos.append(clean_frac.item())
         wrong_memos.append(wrong_frac.item())
@@ -118,13 +189,23 @@ print(f"\ntraining with {noise_label} (seed {seed})\n")
 memo_results = {}
 
 human_labels = noisy_labels[noise_label]
-synth_labels = generate_synthetic_labels(clean_labels, human_labels, seed=seed)
 
 print("training human")
 human = train_and_record_memorization(clean_labels, human_labels, num_epochs)
-print("training synthetic")
+print("training synthetic (same T)")
+synth_labels = generate_synthetic_labels(clean_labels, human_labels, seed=seed)
 synth = train_and_record_memorization(clean_labels, synth_labels, num_epochs)
+print("training pairflip")
+pairflip_labels = generate_pairflip_labels(clean_labels, human_labels, seed=seed)
+pairflip = train_and_record_memorization(clean_labels, pairflip_labels, num_epochs)
+print("training symmetric")
+symmetric_labels = generate_symmetric_labels(clean_labels, human_labels, seed=seed)
+symmetric = train_and_record_memorization(clean_labels, symmetric_labels, num_epochs)
 
 memo_results["human"] = {"clean": human[0], "wrong": human[1]}
 memo_results["synthetic"] = {"clean": synth[0], "wrong": synth[1]}
-torch.save(memo_results, f"memo_results_{noise_label}_{seed}.pt")
+memo_results["pairflip"] = {"clean": pairflip[0], "wrong": pairflip[1]}
+memo_results["symmetric"] = {"clean": symmetric[0], "wrong": symmetric[1]}
+
+# Save the results
+torch.save(memo_results, f"{save_dir}/memo_results_{noise_label}_{seed}.pt")
